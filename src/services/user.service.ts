09dec647e408c7bbf893a95c3c7beb94b0b121fa
Folk Nutnawat <nutnawat.aro@dplusonline.net>
2023-09-12 09:05:26 +0700

    [ADD] - User service

---
+++ src/services/user.service.ts
@@ -0,0 +1,34 @@
+import { IUser } from '@/entities'
+import { apiClient } from '@/libs'
+
+export interface IUserService {
+  changePassword(params: Pick<IUser, 'id' | 'password'>): Promise<void>
+  create(params: Omit<IUser, 'id'>): Promise<Omit<IUser, 'password'>>
+  delete(params: Pick<IUser, 'id'>): Promise<void>
+  getList(): Promise<Omit<IUser, 'password'>[]>
+  getDetail(params: Pick<IUser, 'id'>): Promise<Omit<IUser, 'password'>>
+  update(params: IUser): Promise<Omit<IUser, 'password'>>
+}
+
+export const userService: IUserService = {
+  changePassword: function (params: Pick<IUser, 'id' | 'password'>): Promise<void> {
+    const { id, ...body } = params
+    return apiClient.post(`/users/${id}/password`, body)
+  },
+  create: function (params: Omit<IUser, 'id'>): Promise<Omit<IUser, 'password'>> {
+    return apiClient.post('/users', params)
+  },
+  delete: function (params: Pick<IUser, 'id'>): Promise<void> {
+    return apiClient.delete(`/users/${params.id}`)
+  },
+  getList: function (): Promise<Omit<IUser, 'password'>[]> {
+    return apiClient.get('/users')
+  },
+  getDetail: function (params: Pick<IUser, 'id'>): Promise<Omit<IUser, 'password'>> {
+    return apiClient.get(`/users/${params.id}`)
+  },
+  update: function (params: IUser): Promise<Omit<IUser, 'password'>> {
+    const { id, ...body } = params
+    return apiClient.put(`/users/${id}`, body)
+  },
+}
