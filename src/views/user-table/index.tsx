906439c90affd62ccb04acdf79b994b4700a067b
Folk Nutnawat <nutnawat.aro@dplusonline.net>
2023-09-17 19:45:09 +0700

    [UPDATE] - Move column to constants folder

--- src/views/user-table/index.tsx
+++ src/views/user-table/index.tsx
@@ -1,100 +1,15 @@
 import { DataTable } from '@/components'
+import { getUserTableColumns } from '@/constants'
 import { IUser } from '@/entities'
 import { useAlert, useUser } from '@/hooks'
 import { useMutation, useQuery } from '@tanstack/react-query'
-import { createColumnHelper } from '@tanstack/react-table'
 import React from 'react'
-import { MdDelete, MdEdit, MdLock } from 'react-icons/md'
 
 interface IUserTableViewProps {
   onTriggerChangePassword(params: Pick<IUser, 'id'>): void
   onTriggerCreate(): void
   onTriggerEdit(params: Pick<IUser, 'id'>): void
 }
-
-interface IGetUserTableColumnParams {
-  onTriggerChangePassword(params: Pick<IUser, 'id'>): void
-  onTriggerDelete(params: Pick<IUser, 'id'>): void
-  onTriggerEdit(params: Pick<IUser, 'id'>): void
-}
-
-const columnHelper = createColumnHelper<Omit<IUser, 'password'>>()
-
-const userRoleTagClassName = {
-  ADMIN: 'border-gray-500 bg-gray-50 text-gray-700',
-  OWNER: 'border-gray-500 bg-gray-50 text-gray-700',
-  USER: 'border-gray-500 bg-gray-50 text-gray-700',
-}
-const userStatusTagClassName = {
-  ACTIVE: 'border-green-500 bg-green-50 text-green-700',
-  BANDED: 'border-red-500 bg-red-50 text-red-700',
-  INACTIVE: 'border-gray-500 bg-gray-50 text-gray-700',
-}
-
-const getColumns = ({
-  onTriggerChangePassword,
-  onTriggerDelete,
-  onTriggerEdit,
-}: IGetUserTableColumnParams) => [
-  columnHelper.accessor('username', {
-    cell: (info) => <span className='font-medium'>{info.getValue()}</span>,
-    header: () => <span>Username</span>,
-    size: 800,
-  }),
-  columnHelper.accessor('role', {
-    cell: (info) => (
-      <span
-        className={`px-4 py-1 border rounded-md bg font-semibold text-xs flex items-center w-fit ${
-          userRoleTagClassName[info.getValue()]
-        }`}
-      >
-        {info.getValue()}
-      </span>
-    ),
-    header: () => <span>Role</span>,
-    size: 200,
-  }),
-  columnHelper.accessor('status', {
-    cell: (info) => (
-      <span
-        className={`px-4 py-1 border rounded-md font-semibold text-xs flex items-center w-fit ${
-          userStatusTagClassName[info.getValue()]
-        }`}
-      >
-        {info.getValue()}
-      </span>
-    ),
-    header: () => <span>Status</span>,
-    size: 200,
-  }),
-  columnHelper.accessor('id', {
-    cell: (info) => (
-      <div className='flex items-center justify-center w-full gap-x-2'>
-        <button
-          className='rounded-full w-8 h-8  flex items-center justify-center border  text-sky-500  bg-sky-50 border-sky-500 hover:bg-sky-200'
-          onClick={() => onTriggerEdit({ id: info.getValue() })}
-        >
-          <MdEdit size={16} />
-        </button>
-        <button
-          className='rounded-full w-8 h-8  flex items-center justify-center border  text-yellow-500  bg-yellow-50 border-yellow-500 hover:bg-yellow-200'
-          onClick={() => onTriggerChangePassword({ id: info.getValue() })}
-        >
-          <MdLock size={16} />
-        </button>
-        <button
-          className='rounded-full w-8 h-8 flex items-center justify-center border text-red-500  bg-red-50 border-red-500 hover:bg-red-200'
-          onClick={() => onTriggerDelete({ id: info.getValue() })}
-        >
-          <MdDelete size={16} />
-        </button>
-      </div>
-    ),
-    header: () => <span>Action</span>,
-    size: 100,
-    enableSorting: false,
-  }),
-]
 
 const UserTableView: React.FC<IUserTableViewProps> = ({
   onTriggerChangePassword,
@@ -146,7 +61,7 @@ const UserTableView: React.FC<IUserTableViewProps> = ({
       </div>
       <DataTable
         data={list}
-        columns={getColumns({ onTriggerChangePassword, onTriggerDelete, onTriggerEdit })}
+        columns={getUserTableColumns({ onTriggerChangePassword, onTriggerDelete, onTriggerEdit })}
         globalFilter={globalFilter}
       />
     </div>
